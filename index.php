54df2f91bfa83c874fa6c4993c875781e69fb9f7
wahlf1 <wahlf18@stac.edu>
2022-02-22 20:06:50 -0500

    Initial website setup for the American Restaurant

---
+++ index.php
@@ -0,0 +1,85 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>The American Restaurant</title>
+  <meta name="description" content="The American Restaurant Official Website">
+  <meta name="author" content="Walter Ahlf">
+  <!-- Responsive viewport meta tag -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/main.css?v=1.0">
+
+ 
+
+  <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+  <![endif]-->
+</head>
+
+<body>
+
+  <div class="header">
+    
+  </div>
+
+  <div class="hero-image">
+      <div class="hero-text">
+          <h1>The American Restaurant</h1>
+          <p>Official Site</p>
+        
+      </div>
+  </div>
+
+    <div class="container">
+
+      <div class="row">
+        <div class="col-sm">
+          <a href="#menu"><img src="assets/images/food.jpg" width="100%" /></a>
+        </div>
+        <div class="col-sm">
+            <h2>Experience the best American Food<br></h2>
+         <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. 
+             Separated, they live in Bookmarks Grove right at the coast of the Semantics, a large language ocean. 
+             A small river named Duden flows by their place and supplies it with the necessary regelialia.<br>
+
+            It is a paradisematic country, in which roasted parts of sentences fly into your mouth. 
+            Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life 
+            One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.
+        </p>
+        </div>
+        <div class="col-sm">
+           <a href="#menu"><img src="assets/images/drinks.jpg" width="100%" /></a>
+        </div>
+      </div>
+
+      <br>
+      
+      <div class="row">
+        <div class="col">
+          Left text
+        </div>
+        <div class="col">
+          Right text
+        </div>
+      </div>
+
+    </div><!-- ./container -->
+
+
+   <div class="footer">
+    
+  </div>
+
+  <!-- JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+  <script src="js/scripts.js?v=1.0"></script>
+  <script src="js/dynamic-menu.js"></script>
+</body>
+</html>

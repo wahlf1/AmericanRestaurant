0553cedb532fb8b12e8be2caf51f6a687dd16e06
wahlf1 <wahlf18@stac.edu>
2022-03-22 19:58:54 -0400

    Adding some pages and the dbconnect file

---
+++ includes/dbconnect.php
@@ -0,0 +1,11 @@
+<?php
+
+    $host = "localhost";
+    $username = "";
+    $password = "";
+    $dbname = "restaurant_newsletter";
+
+    //Create the connection
+    $conn = new mysqli($host, $username, $password, $dbname);
+
+?>
